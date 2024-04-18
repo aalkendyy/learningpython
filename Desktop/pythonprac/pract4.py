7733eefbbdef98d6dea18f270cddf13a32d0dc7f
aalkendyy <aishaalkendy9898@gmail.com>
2024-04-18 14:16:37 -0700

    Add pract4.py file

---
+++ Desktop/pythonprac/pract4.py
@@ -0,0 +1,30 @@
+inventory_data = [
+    {"product": "Milk", "sold": 25, "store": "East"},
+    {"product": "Eggs", "sold": 50, "store": "North"},
+    {"product": "Bread", "sold": 20, "store": "East"},
+    {"product": "Milk", "sold": 15, "store": "West"},
+    {"product": "Eggs", "sold": 30, "store": "East"},
+    {"product": "Milk", "sold": 10, "store": "North"}
+]
+
+newlist={}
+for i in inventory_data:
+    product=i["product"]
+    sold=i["sold"]
+    store=i["store"]
+    #print(product,sold,store)
+
+    if product not in newlist:
+        newlist[product]={"amount_sold":0,"store":[]}
+        print(newlist)
+    newlist[product]["amount_sold"]=newlist[product]["amount_sold"]+sold
+
+    if store not in newlist[product]["store"]:
+        newlist[product]["store"].append(store)
+        
+
+for key,value in newlist.items():
+    print(key,value)
+
+
+
